a015d666bee91540effbb3f1c25098d145d68c47
ruixian102 <ruixian@etechnologycentre.com>
2018-04-10 16:07:35 +0800

    minor changed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,110 +2,70 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
 
-                        {{-- <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}"> --}}
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" placeholder="Username" value="{{ old('username') }}" required autofocus>
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                             </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
                             <div class="col-md-6">
-                                <input id="number" type="number" class="form-control" name="number" placeholder="Phone number" value="{{ old('number') }}" required>
-
-                                @if ($errors->has('number'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('number') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="col-md-6">
-                                <input id="company" type="company" class="form-control" name="company" placeholder="Company Name" value="{{ old('company') }}" required>
-
-                                @if ($errors->has('company'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('company') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6">
-                                <input id="billing" type="billing" class="form-control" name="billing" placeholder="Billing Address" value="{{ old('billing') }}" required>
-
-                                @if ($errors->has('billing'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('billing') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="col-md-6">
-                                <input id="shipping" type="shipping" class="form-control" name="shipping" placeholder="Shipping Address" value="{{ old('shipping') }}" required>
-
-                                @if ($errors->has('shipping'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('shipping') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                    <input type="checkbox" name="address" value="same"> My billing address is the same as my shipping address.<br>
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
                             </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm password" required>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div style="text-align:center">
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    {{ __('Register') }}
                                 </button>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div style="text-align:center">
-                                <p>By signing up you agree to our terms and conditions</p>
                             </div>
                         </div>
                     </form>
