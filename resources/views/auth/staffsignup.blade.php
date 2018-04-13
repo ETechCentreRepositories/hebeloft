85738500c948528634bb3ca7d03a5864231d8bd3
ruixian102 <ruixian@etechnologycentre.com>
2018-04-13 09:47:05 +0800

    yes

--- resources/views/auth/staffsignup.blade.php
+++ resources/views/auth/staffsignup.blade.php
@@ -10,16 +10,16 @@
                 <div class="card-header">Staff Sign Up</div>
 
                 <div class="card-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="UsersController@create">
                         {{ csrf_field() }}
 
                         <div class="form-group">
                                 <div class="row">
                             <div class="col-md-6">
-                                <input type="radio" name="gender" value="warehouse"> Warehouse staff<br>
+                                <input id="role" type="radio" name="role" value="5"> Warehouse staff<br>
                             </div>
                             <div class="col-md-6">
-                                <input type="radio" name="gender" value="outlet"> Outlet staff<br>
+                                <input id="role" type="radio" name="role" value="3"> Outlet staff<br>
                             </div>
                                 </div>
                         </div>
@@ -42,7 +42,7 @@
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <input id="email" type="text" class="form-control" name="email" value="enquiry@hebeloft.com" value="{{ old('email') }}" required autofocus>
+                                    <input id="email" type="text" class="form-control" name="email" value="enquiry@hebeloft.com" value="{{ old('email') }}" required>
     
                                     @if ($errors->has('email'))
                                         <span class="help-block">
@@ -56,7 +56,7 @@
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <input id="number" type="number" class="form-control" name="number" placeholder="Phone number" value="{{ old('number') }}" required>
+                                    <input id="phone_number" type="number" class="form-control" name="phone_number" placeholder="Phone number" value="{{ old('number') }}" required>
     
                                     @if ($errors->has('number'))
                                         <span class="help-block">
@@ -89,46 +89,46 @@
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="ogal"> OG ALBERT<br>
+                                    <input type="checkbox" name="outlet" value="ogal"> OG ALBERT<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="ogop"> OG ORCHARD POINT<br>
+                                    <input type="checkbox" name="outlet" value="ogop"> OG ORCHARD POINT<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="ogct"> OG CHINATOWN<br>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="bhgbj"> BHG BISHAN JUNCTION 8<br>
-                                </div>
-                                <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="bhgcck"> BHG CHOA CHU KANG<br>
-                                </div>
-                                <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="bhgjp"> BHG JURONG POINT<br>
+                                    <input type="checkbox" name="outlet" value="ogct"> OG CHINATOWN<br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="metc"> METRO CENTREPOINT<br>
+                                    <input type="checkbox" name="outlet" value="bhgbj"> BHG BISHAN JUNCTION 8<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="metp"> METRO PARAGON<br>
+                                    <input type="checkbox" name="outlet" value="bhgcck"> BHG CHOA CHU KANG<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="nfbt"> NTUC FINEST BUKIT TIMAH<br>
+                                    <input type="checkbox" name="outlet" value="bhgjp"> BHG JURONG POINT<br>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="nfmp"> NTUC FINEST MARINE PARADE<br>
+                                    <input type="checkbox" name="outlet" value="metc"> METRO CENTREPOINT<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="nxjp"> NTUC JURONG POINT XTRA<br>
+                                    <input type="checkbox" name="outlet" value="metp"> METRO PARAGON<br>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="checkbox" name="role" value="nxnc"> NTUC NORTHPOINT CITY<br>
+                                    <input type="checkbox" name="outlet" value="nfbt"> NTUC FINEST BUKIT TIMAH<br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <input type="checkbox" name="outlet" value="nfmp"> NTUC FINEST MARINE PARADE<br>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="checkbox" name="outlet" value="nxjp"> NTUC JURONG POINT XTRA<br>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="checkbox" name="outlet" value="nxnc"> NTUC NORTHPOINT CITY<br>
                                 </div>
                             </div>
                         </div>
