d1cdb82e5dbe8b48a94ae412745535f2d4547cd5
ruixian102 <ruixian@etechnologycentre.com>
2018-04-26 14:27:13 +0800

    merge

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,6 +12,12 @@ window.Vue = require('vue');
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
+import 'jquery-ui/ui/widgets/autocomplete';
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/autocomplete.css';
+import 'jquery-ui/themes/base/theme.css';
+import 'jquery-ui/themes/base/menu.css';
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
